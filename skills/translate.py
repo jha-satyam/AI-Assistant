3c4533070591dc271a2e094c1a746bb6152ff9d0
coldkoffee <satyamjha211@gmail.com>
2023-03-24 18:14:45 +0530

    Removed skills directory

--- skills/translate.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-
-def trans(text):
-    line=str(text)
-    translate=Translator()
-    result=translate.translate(line,language='en')
-    data=result.text
-    print(f"you said:{data}.")
-    return data
