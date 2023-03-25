13f306c00c27adb67f5d43daa23a1a5f4edecbf3
coldkoffee <satyamjha211@gmail.com>
2023-03-25 19:58:14 +0530

    Added mimic3 tts module

---
+++ bin/decompose.py
@@ -0,0 +1,13 @@
+#!/home/anon/Documents/Github/AI-Assistant/bin/python3.10
+
+import unicodedata
+import sys
+
+
+def main(fn):
+    with open(fn, encoding='utf-8') as f:
+        print(unicodedata.normalize('NFD', f.read()))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
